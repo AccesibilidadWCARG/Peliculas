77da95a44d896bc3dd2c6673bf0105647b7fb0fd
GonzaloFuentes <gonza.a.fuentes@gmail.com>
2022-12-13 23:47:09 -0300

    Update WCARG.bat

--- jenkins/scripts/WCARG.bat
+++ jenkins/scripts/WCARG.bat
@@ -1,4 +1,4 @@
-set URL="http://localhost:3000/";
+set URL="http://localhost:3000/"
 set EMAILS="gonza.a.fuentes@gmail.com;rlema.1989@gmail.com"
 git clone https://github.com/AccesibilidadWCARG/WCARG.git
 cd WCARG
